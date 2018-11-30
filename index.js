aa1ab5af217318ca530379f9114644c18ec2744d
Kitayama Noriaki <aspinium@gmail.com>
2018-11-30 09:57:10 -0800

    #4 Change to arrow function

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const http = require("http");
 const os = require("os");
+const PORT = 5012
 
-
-http.createServer(function (request, response) {
-
+http.createServer(
+    //callback
+    (request, response) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end("--------------- CPU ----------------\n"+JSON.stringify(os.cpus())+"\n"+"--------------- ARCHITECTURE ----------------\n"+JSON.stringify(os.arch())+"--------------- PLATFORM ----------------\n"+JSON.stringify(os.platform()));
-
-
+    response.end("-- CPU --\n"
+    +JSON.stringify(os.cpus())+"\n"
+    +"--- ARCHITECTURE ---\n"
+    +os.arch()+"\n"
+    +"--- PLATFORM ---\n"
+    +os.platform());
+    
 }).listen(5012);
 
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:5012/');
+console.log(`Server running at http://127.0.0.1:${PORT}/`);
